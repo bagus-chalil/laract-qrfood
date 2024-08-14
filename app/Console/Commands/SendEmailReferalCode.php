821cbb4929d985451f43a319822e70d9dd893479
baguschalil29 <bagus.chalil@gmail.com>
2024-08-14 10:39:13 +0700

    send mail lani asep

--- app/Console/Commands/SendEmailReferalCode.php
+++ app/Console/Commands/SendEmailReferalCode.php
@@ -33,7 +33,7 @@ class SendEmailReferalCode extends Command
     {
         Log::info("Cron Job started at ". now());
 
-        $user = User::all();
+        $user = User::where('email','asep@kimiafarma.co.id')->get();
 
         foreach ($user as $key => $value) {
             try {
